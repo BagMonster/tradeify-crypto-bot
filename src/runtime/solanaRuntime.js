9fffd6bd240a4d24ff9291026b3a49194531a2f2
Deezy <98153581+BagMonster@users.noreply.github.com>
2026-08-27 07:38:31 -0700

    D-058: restore solanaRuntime.js without the mid-day baseline halt

--- src/runtime/solanaRuntime.js
+++ src/runtime/solanaRuntime.js
@@ -1,1 +1,28 @@
-PLACEHOLDER
+import { evaluateGridRisk } from "../risk/accountRules.js";
+import {
+  LADDER_ACTIONS,
+  accountDayKey,
+  createInitialLadderState,
+  evaluateRiskLadder,
+  markFlattenDone,
+  markPartialCutDone,
+  normalizeLadderState,
+  rollAccountDay,
+  withLadderObservation
+} from "../risk/dailyRiskLadder.js";
+import {
+  GRID_DEFINITION,
+  applyConfirmedEntry,
+  applyConfirmedExit,
+  applyConfirmedProtectiveCut,
+  applySkippedExit,
+  buildProtectiveCutPlan,
+  createInitialSolanaState,
+  entryCandidates,
+  expectedNetUnits,
+  grossVirtualExposureUsd,
+  nextExitAction,
+  normalizeSolanaState,
+  observeRearm,
+  resetAfterProtectiveFlatten
+} from "../strategies/solanaGrid.js";
