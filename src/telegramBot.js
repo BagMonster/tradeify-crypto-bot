d6ac2d267f86156f7eb5e0611e587dd27716215b
Deezy <98153581+BagMonster@users.noreply.github.com>
2026-08-27 18:45:07 -0700

    Restore telegramBot.js and add /b button panel

--- src/telegramBot.js
+++ src/telegramBot.js
@@ -1,1 +1,7 @@
-placeholder
+import TelegramBot from "node-telegram-bot-api";
+import {
+  attachChronicleCommands,
+  CHRONICLE_COMMANDS,
+  CHRONICLE_DEV_BLURB,
+  CHRONICLE_HELP_LINES
+} from "./devCompanionChronicleTelegram.js";
